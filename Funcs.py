62276905cb1d8d098ea4e04d02fbbef46b267172
Andrey <andreivaliev2000@yandex.ru>
2023-06-07 18:07:18 +0300

    task26

--- Funcs.py
+++ Funcs.py
@@ -2,3 +2,16 @@ def revers_range(num):
     if num > 0:
         revers_range(num-1)
     print(num, end="")
+
+
+def degree(a,b):
+    if b==0:
+        return 1
+    if b>0:
+        return a*degree(a,b-1)
+    if b<0: 
+        return 1/a*degree(a,b+1) 
+
+
+
+   
